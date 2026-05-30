d537f85945da756fe27d0efa301a2c2548bc8c17
junners <jose.junryan@gmail.com>
2026-05-29 22:29:35 -0700

    chore: initialize repository

---
+++ src/test/kotlin/ind/junners/practice/PracticeApplicationTests.kt
@@ -0,0 +1,13 @@
+package ind.junners.practice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PracticeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
